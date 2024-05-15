af4dfe69d68186a1da138fae5e1bc709000fdbdc
hugoarmoreira <hugo.moreira.ext@allos.co>
2024-05-15 17:03:05 -0300

    Initial commit

---
+++ .config/lvim/config.lua
@@ -0,0 +1,43 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.colorscheme = "catppuccin-mocha"
+vim.o.termguicolors = true
+
+lvim.keys.normal_mode["<leader>bn"] = ":bnext<CR>"     -- Next buffer
+lvim.keys.normal_mode["<leader>bp"] = ":bprevious<CR>" -- Previous buffer
+lvim.keys.normal_mode["<leader>bf"] = ":bfirst<CR>"    -- First buffer
+lvim.keys.normal_mode["<leader>bl"] = ":blast<CR>"     -- Last buffer
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>Telescope live_grep<CR>', opts)
+
+lvim.builtin.relative_number = true
+vim.opt.relativenumber = true
+
+-- NvimTree configuration to show dotfiles
+lvim.builtin.nvimtree.setup.filters.dotfiles = false
+
+-- Telescope configuration to show dotfiles
+lvim.builtin.telescope.defaults.file_ignore_patterns = {}
+lvim.builtin.telescope.defaults.vimgrep_arguments = {
+  'rg',
+  '--color=never',
+  '--no-heading',
+  '--with-filename',
+  '--line-number',
+  '--column',
+  '--smart-case',
+  '--hidden'  -- Include hidden files
+}
+
+lvim.keys.normal_mode["<leader>n"] = ":nohlsearch<CR>"
+
+lvim.plugins = {
+  { "catppuccin/nvim",        name = "catppuccin" },
+  { "tribela/vim-transparent" },
+  { "github/copilot.vim" }
+}
+
